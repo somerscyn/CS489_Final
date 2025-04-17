e3eb37c47ecd61c037d993c3fc35c94d0787f5cc
Jeymar Guirado <jeymar1013@gmail.com>
2025-04-17 15:18:23 -0400

    womp

--- main.lua
+++ main.lua
@@ -5,12 +5,6 @@ function love.load()
 
     love.window.setTitle("Retro Musashi")
 
-    Push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
-        fullscreen = false,
-        resizable = true,
-        vsync = true
-    })
-
     love.window.setMode(1280, 720, {resizable = true})
     love.graphics.setBackgroundColor(0.1, 0.1, 0.1)
     love.graphics.setColor(1, 1, 1)
@@ -25,11 +19,17 @@ function love.update(dt)
 end
 
 function love.draw()
-    Push:start()
+    --Push:start()
 
     player:draw()
     love.graphics.print("Hello, World!", 400, 300)
 
-    Push:finish()
+    --Push:finish()
 end 
+
+function love.keypressed(key)
+    if key == "escape" then
+        love.event.quit()
+    end
+end
 
