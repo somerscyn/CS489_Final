81f94c9a650fef2cf66057e0c79968694802114a
Jeymar Guirado <jeymar1013@gmail.com>
2025-04-19 15:34:56 -0400

    dqfw

--- src/Globals.lua
+++ src/Globals.lua
@@ -2,5 +2,7 @@ gameWidth = 1280
 gameHeight = 720
 gameState = "start"
 debugFlag = false
+stageX = 50
+stageY = 0
 windowWidth, windowHeight = love.graphics.getDimensions()
 manager = require 'src.stages.stagemanagerinstance'
